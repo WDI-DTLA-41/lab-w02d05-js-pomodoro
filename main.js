8fe20e9614e2c6d5904405776a483160cfb3a141
Jake Miller <hillsighed@gmail.com>
2016-12-09 16:15:14 -0800

    Initial commit with input box, start button and functional display of working on task

---
+++ main.js
@@ -0,0 +1,26 @@
+console.log("hi from js")
+
+// click start button do things
+// will need event listener and function to do things
+$startbtn = document.querySelector('#start-button');
+
+// Function to start Timer
+var startTimer = function (event) {
+  currentTask();
+};
+
+//need input text value
+$task = document.querySelector('input');
+
+
+// Start Button event listener
+$startbtn.addEventListener('click', startTimer);
+
+// function to display currently working on
+var currentTask = function () {
+  $Notification = document.createElement('h1');
+  $Notification.innerHTML = "Currently Working on..." + $task.value;
+  document.body.appendChild($Notification);
+  $task.value = null;
+
+}
