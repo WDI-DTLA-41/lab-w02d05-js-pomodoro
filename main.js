24b540355dbd288c674a5f8e7275ab2e014b3a1c
Kristopher Parrillo <kkparrillo@gmail.com>
2016-12-09 18:54:16 -0800

    working on break

--- main.js
+++ main.js
@@ -2,27 +2,44 @@ console.log("Can't rain all the time");
 
 var btn = document.querySelector('button');
 var pCurrentWork = document.querySelector('p');
-var number = 25; //gonna be 25
+var number = 5; //gonna be 25*60
 var task = document.querySelector('input');
+var timer = document.querySelector('#timer');
+var taskContainer = document.querySelector('.task-container');
 
 
 var handleStart = function(event) {
-  timerId = setInterval(StartTimer, 1000);
-    pCurrentWork.innerHTML = pCurrentWork.textContent + task.value;
-}
+  timerId = setInterval(startTimer, 1000);
+  pCurrentWork.innerHTML = pCurrentWork.textContent + task.value;
+  };
 
 
-var StartTimer = function(event) {
+var startTimer = function(event) {
   var task = document.querySelector('input');
   number = number - 1;
   if (number === 0) {
     clearInterval(timerId) // stop the timer
+    pCurrentWork.innerHTML = "Completed tasks: " + task.value;
+    var takeBreak = document.createElement('p');
+    takeBreak.innerHTML = 'Take a break!';
+    taskContainer.appendChild(takeBreak);
+    timerID = setInterval(startFiveMinTimer, 1000);
   }
-  return console.log(number);
-}
+  timer.textContent = number/60;
+
+};
 
 
+var startFiveMinTimer = function(evt) {
+  var number = 5; // 5 min timer
+  console.log(number);
+  var number = number - 1;
+  timer.textContent = number;
+  if (number === 0) {
+  clearInterval(timerId);
+  }
 
+};
 
 
 
