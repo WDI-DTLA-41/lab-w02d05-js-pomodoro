d4243e3b7602582f4bf723ceaea4c5b82d0368bf
Lacey Madison <laceyannmadison@gmail.com>
2016-12-09 21:55:58 -0800

    mvp functional

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var button = document.querySelector('button');
-var number = 1500000
+var number = 1500000;
 var timerId = null;
 var timeStamp = null;
 
@@ -7,39 +7,29 @@ var getTask = function(evt){
   var inputBox = document.querySelector('input');
   var task = inputBox.value
   document.querySelector('div').innerHTML = 'Currently working on ' + task;
+  inputBox.value = '';
 }
 
 var convertTime = function(number){
-  var milliseconds = number
-
-  var totalSeconds = milliseconds/1000;
-
-  var minutes = totalSeconds/60;
-  minutes = Math.round(minutes);
-
-  var seconds = totalSeconds - minutes * 60;
-  seconds = Math.round(seconds);
-
-  timeStamp = minutes + ':' + seconds;
-
+  return new Date(number).toISOString().slice(14,19);
 }
 
-
-
 var countDown = function(){
-  number = number - 1;
+  number = number - 1000;
+  console.log(number);
   if (number === 0) {
     clearInterval(timerId);
+    return;
   }
-  return number;
+
   var time = convertTime(number);
-  return time;
-  document.querySelector('h2').textContent = number;
+  document.querySelector('h2').textContent = time;
+  console.log(time);
 }
 
 var startTimer = function(evt){
-  console.log('startTimer')
-  timerId = setInterval(countDown, 1000)
+  timerId = setInterval(function(){countDown()}, 1000);
+  button.removeEventListener('click', startTimer);
 }
 
 
