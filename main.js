b50d96dd2b3e4a897bfca567cb9014d7e3484bf6
Jake Miller <hillsighed@gmail.com>
2016-12-09 19:51:34 -0800

    restructured functions

--- main.js
+++ main.js
@@ -9,28 +9,55 @@ $startButton = document.querySelector('#start-button');
 $timeKeeper = document.querySelector('#time-keeper');
 
 
-var displayTaskandTimer = function () {
+var displayTaskandStartTimer = function () {
   if ($inputBox.value === "") {
     return false;
   }
-// Create h1
-$newh1 = document.createElement('h1');
-$newh1.textContent="Currently working on..."
-$timeKeeper.appendChild($newh1)
-
-//create h3
-$newh3 = document.createElement('h3');
-$newh3.setAttribute('id', 'currentTask');
-$newh3.textContent=$inputBox.value;
-$inputBox.value = null;
-$timeKeeper.appendChild($newh3);
-
-// create h2
-$newh2 = document.createElement('h2');
-$newh2.innerHTML = 'Time Left: <span id="timerMinutes">25</span> minutes <span id="timerSeconds">59</span> seconds';
-$timeKeeper.appendChild($newh2);
+  createTaskandTimer();
+  // Start Timer
+  startTimer();
 
 };
 
 // Create Event Listener on Button to create prompt of timer and task
-$startButton.addEventListener('click', displayTaskandTimer);
+$startButton.addEventListener('click', displayTaskandStartTimer);
+
+
+// Create timer
+var startTimer = function () {
+  timerSecondId = setInterval(countDownSeconds,1000);
+}
+
+//timer Second
+var timerMinute = 25;
+var timerSecond = 59;
+var countDownSeconds = function() {
+  $timerSeconds = document.getElementById('timerSeconds');
+  console.log(timerSecond);
+  timerSecond = timerSecond - 1;
+  $timerSeconds.textContent = timerSecond;
+  if (timerSecond === 0) {
+    clearInterval(timerId);
+  }
+  return timerSecond;
+};
+
+// Create Task and timer view
+var createTaskandTimer = function () {
+  // Create h1
+  $newh1 = document.createElement('h1');
+  $newh1.textContent="Currently working on..."
+  $timeKeeper.appendChild($newh1)
+
+  //create h3
+  $newh3 = document.createElement('h3');
+  $newh3.setAttribute('id', 'currentTask');
+  $newh3.textContent=$inputBox.value;
+  $inputBox.value = null;
+  $timeKeeper.appendChild($newh3);
+
+  // create h2
+  $newh2 = document.createElement('h2');
+  $newh2.innerHTML = 'Time Left: <span id="timerMinutes"></span> minutes <span id="timerSeconds"></span> seconds';
+  $timeKeeper.appendChild($newh2);
+};
