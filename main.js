3d921ad09c8bd552e5b9d6b2daf18e19a3cb4ecd
an nguyen <hello.an.ng@gmail.com>
2016-12-09 16:31:21 -0800

    sets variables

--- main.js
+++ main.js
@@ -1,1 +1,55 @@
-console.log("oy");
+console.log("herro");
+
+var input = document.querySelector("input");
+var submit = document.querySelector("button");
+var section = document.querySelector("section");
+var status = document.createElement("p");
+var span = document.createElement("span");
+var timer = document.createElement("div");
+
+
+// ==========================================
+//  ADDS STATUS BAR AND TIMER TO PAGE
+// ==========================================
+
+var handleAddSection = function() {
+
+}
+// CREATE THE SPAN
+
+
+// TAKE THE INPUT VALUE AND UPDATE SPAN TEXT
+
+
+// CREATE THE STATUS BAR
+
+
+// MAKE STATUS TEXT CONTENT = "Currently working on... "
+
+
+// APPEND THE SPAN TO THE STATUS BAR
+
+
+// APPEND THE STATUS TO THE SECTION
+
+
+// CREATE THE TIMER DIV
+
+
+// CHANGE TEXT CONTENT OF TIMER DIV
+
+
+// APPEND THE TIMER DIV TO THE SECTION
+
+
+// RUN THE TIMER
+
+
+// **EVENT LISTENER** TRIGGER WHEN SUBMIT BUTTON IS PUSHED
+
+
+
+
+
+
+
