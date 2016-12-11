a091d2ef495370cc8f9da85cb9f3fd97954e14d2
dakotah walker <walker.dakotah@gmail.com>
2016-12-11 13:00:39 -0800

    clears interval at 00:00 with alert

--- main.js
+++ main.js
@@ -4,36 +4,50 @@ console.log('aloha!');
 // a counter decreasing from 25 minutes
 // ================================================================
 var timer = document.body.querySelector('div');
-var mins = 25; // total amount of minutes to countdown
-var secs = 25 * 60; // coverts total countdown minutes to seconds
+var mins = 1; // total amount of minutes to countdown
+var secs = mins * 60; // coverts total countdown minutes to seconds
 var timerMins = 0; //
 var timerSecs = 0;
+var timerId = null;
 
 // create a function that starts at a given number
 // and counts down by 1 until it reaches 0
 var countDown = function () {
   timerMins = Math.floor(secs / 60);
-  timerSecs = secs % 60;
+  timerSecs = Math.floor(secs % 60);
   // in order to keep clock format (mm:ss),
   // when single mins/secs, print "07" insetad of "7"
-  if(timerSecs <= 9) timerSecs = "0" + timerSecs;
+  if (timerSecs <= 9)
+    timerSecs = "0" + timerSecs;
   secs--;
-  // if (timerId <= 0) {
-  //   return timerId;
-  // } else {
-  // timerId --;
-  // }
+  console.log(secs);
+  if (secs < 0){
+    clearInterval(timerId);
+    alert("Take a break, fool!");
+  }
+  //display timer as "mm:ss"
   timer.innerHTML = timerMins + ":" + timerSecs;
 }
-console.log(countDown());
+
+// ================================================================
+// the timer should stop at OO:00 and alert user to take a break
+// ================================================================
+
+
+  // setTimeout(function(){
+  // console.log("Take a break, fool!");
+  // }, 1000 * secs * mins);
+
 
 // when a user clicks the start button
 // start the countdown
 var btn = document.body.querySelector('button');
+
 var startCountDown = function (){
-  setInterval(countDown, 1000);
+  timerId = setInterval(countDown, 1000);
 }
 btn.addEventListener('click', startCountDown);
+
 
 // ================================================================
 // when a user clicks "start" button, they should see "currently
