be3f2c09d6f5e2f5ede88b7a22e14dd51d85e491
an nguyen <hello.an.ng@gmail.com>
2016-12-12 09:02:20 -0800

    adds current task

--- main.js
+++ main.js
@@ -1,52 +1,67 @@
 console.log("herro");
 
 var input = document.querySelector("input");
-var submit = document.querySelector("button");
+var button = document.querySelector("button");
 var section = document.querySelector("section");
-var status = document.createElement("p");
-var span = document.createElement("span");
-var timer = document.createElement("div");
 
+input.focus();
 
 // ==========================================
 //  ADDS STATUS BAR AND TIMER TO PAGE
 // ==========================================
+var countdown;
 
-var handleAddSection = function() {
+var addStatus = function(event) {
+  console.log("button clicked");
+
+  if (input.value !== "") {
+
+    // CREATE THE SPAN
+    var task = document.createElement("span");
+    task.setAttribute("class", "task");
+
+    // TAKE THE INPUT VALUE AND UPDATE SPAN TEXT
+    task.textContent = input.value;
+
+    // CREATE THE STATUS BAR
+    var status = document.createElement("p");
+    status.setAttribute("class", "status");
+
+    // MAKE STATUS TEXT CONTENT = "Currently working on... "
+    status.textContent = "Currently working on... ";
+
+    // APPEND THE SPAN TO THE STATUS BAR
+    status.appendChild(task);
+
+    // APPEND THE STATUS TO THE SECTION
+    section.appendChild(status);
+
+    // CLEAR THE INPUT FIELD
+    input.value = "";
+    input.focus();
+  }
 
 }
-// CREATE THE SPAN
-
-
-// TAKE THE INPUT VALUE AND UPDATE SPAN TEXT
-
-
-// CREATE THE STATUS BAR
-
-
-// MAKE STATUS TEXT CONTENT = "Currently working on... "
-
-
-// APPEND THE SPAN TO THE STATUS BAR
-
-
-// APPEND THE STATUS TO THE SECTION
-
-
-// CREATE THE TIMER DIV
-
-
-// CHANGE TEXT CONTENT OF TIMER DIV
-
-
-// APPEND THE TIMER DIV TO THE SECTION
-
-
-// RUN THE TIMER
-
-
 // **EVENT LISTENER** TRIGGER WHEN SUBMIT BUTTON IS PUSHED
+button.addEventListener("click", addStatus);
 
+
+var addTimer = function(event) {
+// CREATE THE TIMER DIV
+    var timer = document.createElement("div");
+    timer.setAttribute("class", "timer");
+
+    var countdown = timer;
+    // CHANGE TEXT CONTENT OF TIMER DIV
+
+
+    // APPEND THE TIMER DIV TO THE SECTION
+    section.appendChild(timer);
+
+    // RUN THE TIMER
+
+
+}
 
 
 
