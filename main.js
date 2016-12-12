89900fb3c1444b4646805c6ef52ab158d6a8bff2
Ritwik Ruia <ritwikruia@gmail.com>
2016-12-11 17:47:08 -0800

    adds mvp, minute timer counts down below 0

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ var taskBox = document.querySelector('.dummy');
 var $timer = document.querySelector('.dummytimer');
 var secondsID;
 var seconds = 60;
+var minutesID;
+var minutes = 25;
+
+$inputBox.focus();
+
 //function to be executed on submit button/return key
 var submitClick = function(evt) {
 
@@ -25,22 +30,46 @@ var submitClick = function(evt) {
   $inputBox.value = " ";
 
   // call taskTimer function
-  secondsID = setInterval(taskTimer, 1000);
+  secondsID = setInterval(secondsTimer, 1000); //1000
+  minutesID = setInterval(minutesTimer, 61000); //61000, 60000 was skipping 1 second
+  console.log(secondsID);
+  console.log(minutesID);
+
+  displayID = setInterval(displayTime, 1000); //did not know this should be added
+
 };
 
 $startButton.addEventListener('click', submitClick);
 
 
-var taskTimer = function() {
+var secondsTimer = function() {
   seconds = seconds - 1;
-  if (seconds == -1)
-  {
+  if (seconds === -1) {
     seconds = 59;
   };
-  console.log(seconds);
-  $timer.textContent = seconds;
+
+    console.log(seconds);
+  };
+
+
+
+var minutesTimer = function() {
+  minutes = minutes - 1;
+  if (minutes === 0) {
+    // clearInterval(displayID);
+    clearInterval(minutesID); //added minutes and seconds because minutes was going to -1
+    seconds = 59 //workaround for seconds not stopping at 0 minutes
+     if (minutes === 0 && seconds === 0) { //just added - seconds timer wasnt stopping at 0 minutes
+      clearInterval(displayID); //not working
+      setTimeout(displayID);
+  };
 };
 
+  console.log(minutes);
+};
 
+var displayTime = function() {
+  $timer.textContent = minutes + ":" + seconds;
 
+};
 
