cf9a6ca248cc4dcda82a98006effa351f24bf2ba
Barrett Quan <21barrett@gmail.com>
2016-12-12 01:53:42 -0800

    MVP - break counter

--- main.js
+++ main.js
@@ -8,19 +8,20 @@ var intervalId = window.setInterval(function(){
 //smokescreen clearInterval that stops above code
 window.clearInterval(intervalId);
 
-var minutes = 25;
-var seconds = 60;
+var minutes;
+var seconds;
 var input = document.querySelector('input');
 var taskButton = document.querySelector('.addTask');
 var startButton = document.querySelector('.start');
 var ol = document.querySelector('ol');
 var clockFace = document.querySelector('.countDown');
-clockFace.textContent = (minutes + ":" + seconds);
+var timeOutId;
+var complete = document.querySelector('.complete');
 
 //function to decrease time
-var secondsId = setInterval(function(){
-  console.log(seconds -= 1);
-}, 1000);
+// var secondsId = setInterval(function(){
+//   console.log(seconds -= 1);
+// }, 1000);
 
 //turns text typed into input to tasks on HTML
 var handleInput = function(event) {
@@ -34,6 +35,31 @@ var handleInput = function(event) {
   };
 }
 
+//create time function (25 mins = 1500 seconds * 1000 milliseconds)
+var twentyFiveMins = function(mins) {
+  seconds = 60;
+  minutes = mins - 1;
+
+  var minutesId = setInterval(function(){
+  minutes -= 1;
+  clockFace.textContent = (minutes + ":" + seconds);
+  }, 60000);
+
+  var secondsId = setInterval(function(){
+  seconds = seconds - 1;
+  clockFace.textContent = (minutes + ":" + seconds);
+  }, 1000);
+
+  timeoutId = setTimeout(function(seconds){
+    minutes = 00;
+    seconds = 00;
+    clearInterval(secondsId);
+    clearInterval(minutesId);
+    clockFace.textContent = (minutes + ":" + seconds);
+  }, 25 * 60000)
+}
+
+
 //when click on add task button, adds task to list
 var handleClick = function(event) {
   if (input.value !== "") {
@@ -42,10 +68,26 @@ var handleClick = function(event) {
     ol.appendChild(li);
     input.value = "";
   }
+
+  if (event.target === startButton) {
+    twentyFiveMins(25);
+  }
 // console.log('clicked');
 }
+
+var crossOut = function(event){
+  console.log('clicked on list')
+event.target.classList.toggle('active');
+};
+
+var numComplete = function() {
+  var active = document.querySelectorAll('.active');
+  complete.textContent = active.length + ' tasks completed';
+}
+
+ol.addEventListener('click', crossOut);
+ol.addEventListener('click', numComplete);
 taskButton.addEventListener('click', handleClick);
 input.addEventListener('keypress', handleInput);
-
-
+startButton.addEventListener('click', handleClick);
 
