8e7237b10905e42036f7b719ed9786e20ed0f19c
Tim Clark <oeomusic@gmail.com>
2016-12-12 20:38:22 -0800

    sets countdown back to 1000ms

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ var timerId = null;
 var minutes = 25;
 var seconds = 60;
 
+var currentTimer = '';
+
 var countDown = function() {
   seconds -= 1;
   if (seconds < 10) {
@@ -29,19 +31,64 @@ var countDown = function() {
     $seconds.innerHTML = '00';
     clearInterval(timerId);
     $startBtn.addEventListener('click', startTimer);
+    // breakTimer();
+
   }
   console.log('minute' + minutes);
   console.log('seconds' + seconds);
 }
 
+// var breakCount = function() {
+//   seconds -= 1;
+//   if (seconds < 10) {
+//     $seconds.innerHTML = '0' + seconds;
+//   } else {
+//     $seconds.innerHTML = seconds;
+//   }
+//   if (seconds === 0) {
+//     seconds = 60;
+//   }
+//   if (seconds === 59) {
+//     minutes -= 1;
+//   }
+//     $minutes.innerHTML = '0' + minutes;
+//    if (minutes === 0 && seconds === 1) {
+//     $seconds.innerHTML = '00';
+//     clearInterval(newTimerId);
+//     $startBtn.addEventListener('click', startTimer);
+
+//   }
+//   console.log('minute' + minutes);
+//   console.log('seconds' + seconds);
+// }
+
+
 
 var startTimer = function(event) {
   $workingOn.textContent = ' ' + $task.value;
   $workingOn.classList.add('workin');
+  $task.value = '';
   minutes = 25;
   seconds = 60;
   timerId = setInterval(countDown, 10);
   $startBtn.removeEventListener('click', startTimer);
 }
 
+// var breakTimer = function(event) {
+//   $workingOn.textContent = 'Take a Break!';
+//   minutes = 5;
+//   seconds = 60;
+//   newTimerId = setInterval(breakCount, 10);
+//   $startBtn.removeEventListener('click', breakCount);
+// }
+
 $startBtn.addEventListener('click', startTimer);
+
+
+
+
+
+
+
+
+
